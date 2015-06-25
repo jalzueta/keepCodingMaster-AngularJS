c1976d1dd18d8f9a6f33cdeb3196ef9f7904c047
Javi Alzueta <jalzuetap@gmail.com>
2015-06-25 15:18:05 +0200

    Implementado el filtrado de géneros por nombre

--- scripts/controllers/GenresCtrl.js
+++ scripts/controllers/GenresCtrl.js
@@ -4,8 +4,29 @@
 
 angular
     .module("jeviteca")
-    .controller("GenresCtrl", ["$scope", "Genres", function( $scope, Genres ){
+    .controller("GenresCtrl", ["$scope", "Genres", "ApiService", "Settings", function( $scope, Genres, ApiService, Settings ){
 
         $scope.genres = Genres.data;
 
+        $scope.$watch(
+            function () {
+                return $scope.filtroGenreValue;
+            },
+            function (newValue, oldValue) {
+                if(newValue === "" || newValue === undefined){
+                    $scope.genres = Genres.data;
+                }else{
+                    ApiService.getFilteredData(Settings.genresUrl, [{name: newValue}]).then(
+                        function (filteredData) {
+                            var uniqueFilteredData = filteredData.filter(function(item, pos) {
+                                return filteredData.indexOf(item) == pos;
+                            });
+                            $scope.genres = uniqueFilteredData;
+                        },
+                        function (error) {
+                            console.log('Ha ocurrido un error');
+                        });
+                }
+            }
+        );
     }]);
