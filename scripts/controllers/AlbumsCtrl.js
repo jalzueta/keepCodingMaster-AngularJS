c57a6960a25a644c065b6dbf5cc8a751dba2b88d
Javi Alzueta <jalzuetap@gmail.com>
2015-06-25 15:15:26 +0200

    Implementad funcionalidad de filtrado de bandas. Pulido el codigo del filtrado de albumes

--- scripts/controllers/AlbumsCtrl.js
+++ scripts/controllers/AlbumsCtrl.js
@@ -3,29 +3,23 @@
  */
 angular
     .module("jeviteca")
-    .controller("AlbumsCtrl", ["$scope", "Albums", "ApiService", "Settings", "$q", function( $scope, Albums, ApiService, Settings, $q ){
+    .controller("AlbumsCtrl", ["$scope", "Albums", "ApiService", "Settings", function( $scope, Albums, ApiService, Settings ){
 
         $scope.albums = Albums.data;
 
         $scope.$watch(
-            // Hacemos un return del valor que queremos observar
             function () {
                 return $scope.filtroAlbumValue;
             },
-            // Manejador que se dispara cuando hay cambios
             function (newValue, oldValue) {
-                window.console.log("Valor antiguo: " + oldValue);
-                window.console.log("Valor nuevo: " + newValue);
                 if(newValue === "" || newValue === undefined){
                     $scope.albums = Albums.data;
                 }else{
-                    var filteredData = Array();;
                     ApiService.getFilteredData(Settings.albumsUrl, [{title: newValue}, {band: {name: newValue}}, {year: parseInt(newValue)}]).then(
-                        function (data) {
-                            filteredData = filteredData.concat(data);
+                        function (filteredData) {
                             var uniqueFilteredData = filteredData.filter(function(item, pos) {
                                 return filteredData.indexOf(item) == pos;
-                            })
+                            });
                             $scope.albums = uniqueFilteredData;
                         },
                         function (error) {
--- scripts/controllers/BandsCtrl.js
+++ scripts/controllers/BandsCtrl.js
@@ -4,9 +4,30 @@
 
 angular
     .module("jeviteca")
-    .controller("BandsCtrl", ["$scope", "Bands", function( $scope, Bands ){
+    .controller("BandsCtrl", ["$scope", "Bands", "ApiService", "Settings", function( $scope, Bands, ApiService, Settings ){
 
         $scope.bands = Bands.data;
 
+        $scope.$watch(
+            function () {
+                return $scope.filtroBandValue;
+            },
+            function (newValue, oldValue) {
+                if(newValue === "" || newValue === undefined){
+                    $scope.bands = Bands.data;
+                }else{
+                    ApiService.getFilteredData(Settings.bandsUrl, [{name: newValue}]).then(
+                        function (filteredData) {
+                            var uniqueFilteredData = filteredData.filter(function(item, pos) {
+                                return filteredData.indexOf(item) == pos;
+                            });
+                            $scope.bands = uniqueFilteredData;
+                        },
+                        function (error) {
+                            console.log('Ha ocurrido un error');
+                        });
+                }
+            }
+        );
     }]);
 
